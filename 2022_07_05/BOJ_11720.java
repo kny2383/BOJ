f061afee0cd0c6b9387454220a1da140e7920f0c
kny2383 <kny2383@gmail.com>
2022-07-05 08:33:53 +0900

    [BOJ]_11720_숫자의합.java Edit

--- 2022_07_05/BOJ_11720.java
+++ 2022_07_05/BOJ_11720.java
@@ -1,4 +1,18 @@
+import java.util.Scanner;
 
-public class BOJ_11720 {
-
+public class Main {
+	public static void main(String[] args) { 
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt(); // 숫자의 개수
+		String a = sc.next(); // 더할 숫자
+		sc.close();
+		int sum = 0; // 합
+		
+		for(int i = 0; i < n; i++) {
+			sum += a.charAt(i)-'0'; 
+			// charAt()은 해당 문자의 아스키코드 값을 반환해서 반드시 -48 또는 -'0'을 해줘야 입력받은 숫자 값을 그대로 사용할 수 있다.
+		}
+		System.out.println(sum);
+	}
 }
