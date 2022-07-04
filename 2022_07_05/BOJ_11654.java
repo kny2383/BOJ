0ba403204379fed08bcfc65fa625853eed68a980
kny2383 <kny2383@gmail.com>
2022-07-05 07:49:30 +0900

    [BOJ]_11654_아스키코드.java

---
+++ 2022_07_05/BOJ_11654.java
@@ -0,0 +1,8 @@
+import java.io.IOException;
+
+public class BOJ_11654 {
+	public static void main(String[] args) throws IOException { // throws IOException -> 입출력 명령어 사용시 예외처리를 위해
+		int num = System.in.read(); // int를 통해 숫자나 단일 문자 char로 값을 하나씩 가져온다
+		System.out.println(num);
+	}
+}
